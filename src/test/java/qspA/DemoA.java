7a8ccdc142fa7b11a714a600d49f836013dc4113
APM-SYSTEM <APM-SYSTEM@DESKTOP-488FP15>
2019-07-27 13:37:15 +0530

    1st project

---
+++ src/test/java/qspA/DemoA.java
@@ -0,0 +1,14 @@
+package qspA;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("selenium",true);
+	}
+
+}
